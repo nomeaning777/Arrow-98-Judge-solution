b9d5414d0409287ec09aac56e0365e5c38580d4e
nomeaning <nomeaning777@gmail.com>
2012-04-03 03:29:45 +0900

    Modified Problem1009

--- Prob1009.rb
+++ Prob1009.rb
@@ -1,50 +1,18 @@
+Tokenizer = [[/^[()]/, "PAREN"], [/^[\[\]]/, "BRACK"], [/^[\{\}]/m, "BRACE"],
+             [/^[:;,]/, "COLON"], [/^'((\\.)|[^'])*'/, "CHARA"], [/^"((\\.)|[^"])*"/, "STRNG"],
+			 [/^[a-zA-Z_][0-9a-zA-Z_]*/, "IDENT"], [/^[0-9]+/, "INTGR"], [/^#.*$/, "PREPR"],
+			 [/^(\+\+|--|==|!=|\|\||&&)/, "OPR2D"], [/^[\+-=!\|&\*\/]/ ,"OPR1D"]]
 while line = gets
 	while line.length > 0
-		c = line[0]
-		cc = ""
-		if line.length > 1
-			cc = line[0..1]
+		any_match = nil
+		Tokenizer.each do |token|
+			if token[0] =~ line
+				m = token[0].match(line)
+				puts "#{token[1]} #{m[0]}"
+				line=line[m[0].length-1..line.length-1]
+				break
+			end
 		end
-		if c == "(" || c == ")"
-			puts "PAREN #{c}"
-		elsif c == "[" || c == "]"
-			puts "BRACK #{c}"
-		elsif c == "{" || c == "}"
-			puts "BRACE #{c}"
-		elsif c == ":" || c == ";" || c== ","
-			puts "COLON #{c}"
-		elsif c == "'"
-			re = /'((\\.)|[^'])*'/ 
-			m = re.match(line)
-			puts "CHARA #{m[0]}"
-			line = line[m[0].length-1..line.length-1]
-		elsif c == '"'
-			re = /"((\\.)|[^"])*"/ 
-			m = re.match(line)
-			puts "STRNG #{m[0]}"
-			line = line[m[0].length-1..line.length-1]
-		elsif /[a-zA-Z_]/ =~ c
-			re = /[a-zA-Z_][0-9a-zA-Z_]*/
-			m = re.match(line)
-			puts "IDENT #{m[0]}"
-			line = line[m[0].length-1..line.length-1]
-		elsif /[0-9]/ =~ c
-			re = /[0-9]+/
-			m = re.match(line)
-			puts "INTGR #{m[0]}"
-			line = line[m[0].length-1..line.length-1]
-		elsif c == "#"
-			re = /#.*$/
-			m = re.match(line)
-			puts "PREPR #{m[0]}"
-			line = line[m[0].length-1..line.length-1]
-		elsif ["++","--","==","!=","||","&&"].index(cc)
-			puts "OPR2D #{cc}"
-			line = line[1..line.length-1]
-		elsif ["+","-","=","!","|","&","*","/"].index(c)
-			puts "OPR1D #{c}"
-		end
-		line = line[1..line.length-1]
+		line=line[1..line.length-1]
 	end
-
 end
